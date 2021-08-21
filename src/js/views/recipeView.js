c0b923c7fe633551f8a64f9a64e915467c8d409b
Paweł <pglmotyka@gmail.com>
2021-08-21 21:46:02 +0200

    fix fraction package

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 // import icons from '../img/icons.svg';  // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
+import Fraction from '../../../../starter/node_modules/fraction.js/fraction.js';
 import View from './View';
 
 class RecipeView extends View {
@@ -135,7 +136,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
